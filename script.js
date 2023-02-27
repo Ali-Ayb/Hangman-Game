92d9ddf66fcf1ad5f207834cf541dbe9955a49fa
Ali-Ayb <aliayoub.me1@gmail.com>
2023-02-27 23:39:55 +0200

    added hidden class to body parts

---
+++ script.js
@@ -0,0 +1,14 @@
+const hangman = document.getElementById("hangman");
+const head = document.getElementById("head");
+const body = document.getElementById("body");
+const leftArm = document.getElementById("left-arm");
+const rightArm = document.getElementById("right-arm");
+const leftLeg = document.getElementById("left-leg");
+const rightLeg = document.getElementById("right-leg");
+
+head.classList.add("hidden");
+body.classList.add("hidden");
+leftArm.classList.add("hidden");
+rightArm.classList.add("hidden");
+leftLeg.classList.add("hidden");
+rightLeg.classList.add("hidden");
